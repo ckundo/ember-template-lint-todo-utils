4cab9ba6edb60e279810b02d6b99c142749154fd
Cameron Cundiff <ccundiff@linkedin.com>
2021-08-24 16:04:53 -0400

    Describe tests more clearly

--- __tests__/conflict-test.ts
+++ __tests__/conflict-test.ts
@@ -2,25 +2,40 @@ import { resolveConflict } from '../src/git-utils';
 import { readFileSync } from 'fs';
 
 describe('merge conflict resolution', () => {
-  describe('with an addition and a deletion', () => {
-    it('resolves the conflict', () => {
-      const conflict = readFileSync('__tests__/__fixtures__/lockfiles/.lint-todo.additions-removals.conflict.lock', 'utf-8');
-      const expected = readFileSync('__tests__/__fixtures__/lockfiles/.lint-todo.additions-removals.resolved.lock', 'utf-8');
+  describe('given a branch that a 2 todos', () => {
+    describe('merging a branch with empty todos', () => {
+      it('results in a file with 2 todos', () => {
+        const conflict = readFileSync(
+          '__tests__/__fixtures__/lockfiles/.lint-todo.additions-removals.conflict.lock',
+          'utf-8'
+        );
+        const expected = readFileSync(
+          '__tests__/__fixtures__/lockfiles/.lint-todo.additions-removals.resolved.lock',
+          'utf-8'
+        );
 
-      const resolved = resolveConflict(conflict);
-
-      expect(resolved).toEqual(expected);
+        const resolved = resolveConflict(conflict);
+        expect(resolved).toEqual(expected);
+      });
     });
   });
 
-  describe('with multiple additions and deletions', () => {
-    it('resolves the conflict', () => {
-      const conflict = readFileSync('__tests__/__fixtures__/lockfiles/.lint-todo.multiple-additions.conflict.lock', 'utf-8');
-      const expected = readFileSync('__tests__/__fixtures__/lockfiles/.lint-todo.multiple-additions.resolved.lock', 'utf-8');
+  describe('given a branch that 1 todo', () => {
+    describe('merging a branch that updates that same todo', () => {
+      it('resolved the conflict with the current changes (HEAD)', () => {
+        const conflict = readFileSync(
+          '__tests__/__fixtures__/lockfiles/.lint-todo.multiple-additions.conflict.lock',
+          'utf-8'
+        );
+        const expected = readFileSync(
+          '__tests__/__fixtures__/lockfiles/.lint-todo.multiple-additions.resolved.lock',
+          'utf-8'
+        );
 
-      const resolved = resolveConflict(conflict);
+        const resolved = resolveConflict(conflict);
 
-      expect(resolved).toEqual(expected);
+        expect(resolved).toEqual(expected);
+      });
     });
   });
 });
